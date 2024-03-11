ded454f8610c95dd5b941144b1fe57b63263dbd7
serwis <pocitace.lbc@gmail.com>
2024-03-11 21:48:27 +0100

    01 Git-Hub

---
+++ js/01-main.js
@@ -0,0 +1,7 @@
+//Klient na stránce zadá své jméno a spustí se alert,
+//kde bude pozdrav s jeho jménem: "Ahoj XXX!!!"
+
+
+let jmenouzivatele = prompt("Vaše jméno?");
+    alert("Ahoj "+ jmenouzivatele);
+    console.log(jmenouzivatele);    
